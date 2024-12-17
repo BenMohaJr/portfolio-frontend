543314962e41cf74af09dee65dc609b941e15cd9
study <study@MacBook-Pro-2.local>
2024-12-17 18:36:17 +0200

    Deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/BenMohaJr.github.io',
+  assetPrefix: '/BenMohaJr.github.io/',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
